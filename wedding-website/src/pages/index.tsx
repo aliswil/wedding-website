1b71060c6f186ffb239ef9a0027aced0c216cc3d
alis.wilson@norgesgruppen.no <aliswkn@gmail.com>
2023-03-20 22:00:19 +0100

    small fix

--- wedding-website/src/pages/index.tsx
+++ wedding-website/src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Hero heading="Henriette og Sivert" message="17. juni 2023" />
+      <Hero heading="Henriette og Sivert!" message="17. juni 2023" />
     </div>
   );
 }
