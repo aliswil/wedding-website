e0805f743b1b7131b4125d7c79317bd7d1f9a371
aliswil <aliswkn@gmail.com>
2023-04-11 22:56:42 +0200

    aligning correctly

--- wedding-website/src/components/Hero.tsx
+++ wedding-website/src/components/Hero.tsx
@@ -12,10 +12,12 @@ const Hero = () => {
     <div className="flex items-center sm:justify-start justify-center sm:pl-32 h-screen bg-cover bg-center bg-bg-mobile sm:bg-bg-bigscreen ">
       <div className="absolute top-0 left-0 right-0 h-screen bottom-0 bg-black/70 z-[2]" />
       <div className="p-5 text-white z-[2] mt-[-10rem]">
-        <h2 className=" text-8xl font-bold font-mark">Henriette og Sivert</h2>
+        <h2 className="text-7xl sm:text-8xl font-bold font-mark">
+          Henriette og Sivert
+        </h2>
         <p className="py-3 text-4xl font-display pt-8">17. juni 2023</p>
-        <div className="pt-52 text-4xl font-display">
-          <button className="px-8 py-3 border w-full" onClick={handleClick}>
+        <div className="pt-52 text-4xl font-display flex justify-center sm:justify-start">
+          <button className="px-8 py-3 border sm:w-full" onClick={handleClick}>
             Påmelding
           </button>
         </div>
