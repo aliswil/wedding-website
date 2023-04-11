5606c96e8456f810ee54063b6f9501c98dc5b1be
aliswil <aliswkn@gmail.com>
2023-04-11 22:16:31 +0200

    height of image fix

--- wedding-website/src/components/Hero.tsx
+++ wedding-website/src/components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
 
   return (
     <div className="flex items-center justify-center h-screen bg-cover bg-center bg-bg-mobile sm:bg-bg-bigscreen ">
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]" />
+      <div className="absolute top-0 left-0 right-0 h-screen bottom-0 bg-black/70 z-[2]" />
       <div className="p-5 text-white z-[2]  mt-[-10rem]">
         <h2 className=" text-7xl font-bold font-mark">Henriette og Sivert</h2>
         <p className="py-5 text-xl font-display">17. juni 2023</p>
